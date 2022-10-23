46fcdf9a6b8f3532e6ad70a77cbe40048b37bc70
Talha Mirza <tmirza43@gmail.com>
2022-10-23 13:56:07 -0700

    commit1

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 # ******************************
 # Make your Code
 # ******************************
+
+numbers = []
+for i in range(5):
+  numbers.append(int(input("Enter num: ")))
+
+for n in range(5):
+  if numbers[n] > (sum(numbers)/5):
+    print(numbers[n], end = ' ')
